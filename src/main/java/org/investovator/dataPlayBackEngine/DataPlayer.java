61835dc92d16deaa6a4e7c8ddce1798b759423e7
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-14 23:36:00 +0530

    Completed the getOHLCPrice() method. Couldn't test it

--- src/main/java/org/investovator/dataPlayBackEngine/DataPlayer.java
+++ src/main/java/org/investovator/dataPlayBackEngine/DataPlayer.java
@@ -1,6 +1,10 @@
 package org.investovator.dataPlayBackEngine;
 
 import org.investovator.core.data.HistoryDataAPI;
+import org.investovator.core.data.api.CompanyStockTransactionsData;
+import org.investovator.core.data.api.utils.StockTradingData;
+import org.investovator.core.data.api.utils.TradingDataAttribute;
+import org.investovator.core.data.exeptions.DataAccessException;
 import org.investovator.core.excelimporter.HistoryData;
 import org.investovator.dataPlayBackEngine.data.BogusHistoryDataGenerator;
 import org.investovator.dataPlayBackEngine.scheduler.EventTask;
@@ -8,6 +12,7 @@ import org.investovator.dataPlayBackEngine.scheduler.EventTask;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Observer;
 import java.util.Timer;
 
@@ -19,13 +24,19 @@ public class DataPlayer {
 
     Timer timer;
     EventTask task;
-    HistoryDataAPI dataAPI;
+    CompanyStockTransactionsData dataAPI;
+
+
+   //to cache the stock trading data items
+    HashMap<String,HashMap<Date, HashMap<TradingDataAttribute, Float>>> ohlcDataCache;
 
     public DataPlayer(String stock) {
         this.timer = new Timer();
         //for testing
         this.dataAPI=new BogusHistoryDataGenerator();
         task = new EventTask(stock, "2011-12-13-15-55-32",dataAPI);
+
+        ohlcDataCache=new HashMap<String, HashMap<Date, HashMap<TradingDataAttribute, Float>>>();
 
     }
 
@@ -42,9 +53,10 @@ public class DataPlayer {
         timer.cancel();
     }
 
-    public HistoryData getOHLCPrice(String stock, String date){
+    public float getOHLCPrice(String stock, String date){
 
         Date currentTime=null;
+        float price=0;
 
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-kk-mm-ss"); //should be in format year-month-date-24hr-minute-second
         try {
@@ -53,6 +65,41 @@ public class DataPlayer {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return dataAPI.getOHLCPData(currentTime,stock);
+
+        boolean inCache=false;
+
+        //if the stocks data is already in the cache
+        if(ohlcDataCache.containsKey(stock)){
+            //if the requested date is in the cache
+            if(ohlcDataCache.get(stock).containsKey(currentTime)){
+                inCache=true;
+                //just the closing price is enough for now
+                price=ohlcDataCache.get(stock).get(currentTime).get(TradingDataAttribute.CLOSING_PRICE);
+
+            }
+
+        }
+
+        //if the stock data is not in the cache
+        if(!inCache){
+            //define the attributes needed
+            TradingDataAttribute attributes[]=new TradingDataAttribute[2];
+
+            //just the closing price is enough for now
+            attributes[0]=TradingDataAttribute.DAY;
+            attributes[1]=TradingDataAttribute.CLOSING_PRICE;
+
+            //let's take the next 100 of rows
+            try {
+                StockTradingData data=dataAPI.getTradingDataOHLC(stock,currentTime,attributes,100);
+                ohlcDataCache.put(stock,data.getTradingData());
+                price=ohlcDataCache.get(stock).get(currentTime).get(TradingDataAttribute.CLOSING_PRICE);
+            } catch (DataAccessException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+        }
+
+        return price;
     }
 }
