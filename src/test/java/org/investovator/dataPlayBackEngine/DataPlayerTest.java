42be5083294701d4a589771c71a6eddd3bb993bb
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-16 22:54:11 +0530

    Added a test for the company list retriever

--- src/test/java/org/investovator/dataPlayBackEngine/DataPlayerTest.java
+++ src/test/java/org/investovator/dataPlayBackEngine/DataPlayerTest.java
@@ -1,12 +1,30 @@
 package org.investovator.dataPlayBackEngine;
 
+import org.investovator.dataPlayBackEngine.data.BogusCompnayDataGenerator;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 /**
  * @author: ishan
  * @version: ${Revision}
  */
 public class DataPlayerTest {
+
+    DataPlayer player;
+
+    @Before
+    public void setUp() throws Exception {
+
+        String[] stocks=new String[2];
+        stocks[0]="GOOG";
+        stocks[1]="APPL";
+        player=new DataPlayer(stocks);
+
+
+    }
+
     @Test
     public void testRunPlayback() throws Exception {
 
@@ -19,10 +37,7 @@ public class DataPlayerTest {
 
     @Test
     public void testGetOHLCPrice() throws Exception {
-        String[] stocks=new String[2];
-        stocks[0]="GOOG";
-        stocks[1]="APPL";
-        DataPlayer player=new DataPlayer(stocks);
+
 
         for(int a=0;a<5;a++){
             float data=player.getOHLCPrice("G","2012-10-3-19-45-3"+Integer.toString(a));
@@ -33,5 +48,19 @@ public class DataPlayerTest {
 
 
 
+    }
+
+    @Test
+    public void testGetStocksList() throws Exception{
+        HashMap<String, String> map= player.getStocksList();
+
+        for(String stock:map.keySet()){
+            if(stock.equalsIgnoreCase("APPL")){
+                assert (map.get(stock).equalsIgnoreCase("Apple Computers"));
+            }
+            else if(stock.equalsIgnoreCase("GOOG")){
+                assert (map.get(stock).equalsIgnoreCase("Google Lanka"));
+            }
+        }
     }
 }
