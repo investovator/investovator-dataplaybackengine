ad1bf3990baa7d0eb2a9cd4a48224f4cc33d851e
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-27 20:17:51 +0530

    Fixed a bug of not properly terminating the game

--- src/main/java/org/investovator/dataplaybackengine/scheduler/RealTimeEventTask.java
+++ src/main/java/org/investovator/dataplaybackengine/scheduler/RealTimeEventTask.java
@@ -19,7 +19,7 @@ import java.util.*;
 public class RealTimeEventTask extends TimerTask {
 
     //used to determine the cache size
-    public static int CACHE_SIZE=100;
+    public static int CACHE_SIZE = 100;
 
     //start time
     private Date currentTime;
@@ -39,23 +39,23 @@ public class RealTimeEventTask extends TimerTask {
 
     private RealTimeEventTask(String[] stocksToWatch, CompanyStockTransactionsData api,
                               ArrayList<TradingDataAttribute> attributes) {
-        this.dataApi=api;
-        eventManager=new EventManager();
-        this.attributes=attributes;
+        this.dataApi = api;
+        eventManager = new EventManager();
+        this.attributes = attributes;
 
 
-        Comparator<StockUpdateEvent> comparator=new StockEventComparator();
-        dataCache =new PriorityQueue<StockUpdateEvent>(RealTimeEventTask.CACHE_SIZE,comparator );
-        stocks=new ArrayList<String>(Arrays.asList(stocksToWatch));
+        Comparator<StockUpdateEvent> comparator = new StockEventComparator();
+        dataCache = new PriorityQueue<StockUpdateEvent>(RealTimeEventTask.CACHE_SIZE, comparator);
+        stocks = new ArrayList<String>(Arrays.asList(stocksToWatch));
 
     }
 
     public RealTimeEventTask(String[] stocksToWatch, String startT, String dateFormat, CompanyStockTransactionsData api,
                              ArrayList<TradingDataAttribute> attributes) {
-        this(stocksToWatch,api,attributes);
+        this(stocksToWatch, api, attributes);
 
         try {
-            currentTime=DateUtils.dateStringToDateObject(startT,dateFormat);
+            currentTime = DateUtils.dateStringToDateObject(startT, dateFormat);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -64,19 +64,19 @@ public class RealTimeEventTask extends TimerTask {
 
     public RealTimeEventTask(String[] stocksToWatch, Date startT, CompanyStockTransactionsData api,
                              ArrayList<TradingDataAttribute> attributes) {
-        this(stocksToWatch,api,attributes);
+        this(stocksToWatch, api, attributes);
 
-            currentTime=startT;
+        currentTime = startT;
 
     }
 
     @Override
     public void run() {
 
-        boolean inCache=fireEvents();
+        boolean inCache = fireEvents();
         //if no events were fired
         //TODO - this part would get called even if there was actually no stock events in the dataset in that time
-        if(!inCache){
+        if (!inCache) {
             try {
                 refreshCache();
             } catch (GameFinishedException e) {
@@ -88,11 +88,11 @@ public class RealTimeEventTask extends TimerTask {
             fireEvents();
         }
         // in order to point to the next time interval
-        currentTime= DateUtils.incrementTimeBySeconds(1, currentTime);
+        currentTime = DateUtils.incrementTimeBySeconds(1, currentTime);
 
     }
 
-    public void setObserver(PlaybackEventListener observer){
+    public void setObserver(PlaybackEventListener observer) {
         eventManager.addObserver(observer);
     }
 
@@ -100,70 +100,73 @@ public class RealTimeEventTask extends TimerTask {
     private void refreshCache() throws GameFinishedException {
 
         //to track whether at least a single stock has data in the future
-        boolean hasData=false;
+        boolean hasData = false;
 
-            //for each stock, search for events
-            for(String stock:stocks){
+        //for each stock, search for events
+        for (String stock : stocks) {
 
-                try {
-                    //todo- Assumed that the maximum resolution of "time" for the data in the data base is 1 second
-                    StockTradingData data=dataApi.getTradingData(CompanyStockTransactionsData.DataType.TICKER,
-                            stock,currentTime,null, RealTimeEventTask.CACHE_SIZE,attributes);
+            try {
+                //todo- Assumed that the maximum resolution of "time" for the data in the data base is 1 second
+                StockTradingData data = dataApi.getTradingData(CompanyStockTransactionsData.DataType.TICKER,
+                        stock, currentTime, null, RealTimeEventTask.CACHE_SIZE, attributes);
 
-                    HashMap<Date, HashMap<TradingDataAttribute, String>> stockData= data.getTradingData();
+                HashMap<Date, HashMap<TradingDataAttribute, String>> stockData = data.getTradingData();
 
-                    //add each event to the cache
-                    for(Date time:stockData.keySet()){
-                        StockUpdateEvent event= new StockUpdateEvent(stock,stockData.get(time),time);
-                        dataCache.add(event);
-                    }
-
-                    hasData=true;
-
-                } catch (DataNotFoundException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (DataAccessException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                //add each event to the cache
+                for (Date time : stockData.keySet()) {
+                    StockUpdateEvent event = new StockUpdateEvent(stock, stockData.get(time), time);
+                    dataCache.add(event);
                 }
 
+                hasData = true;
 
+            } catch (DataNotFoundException e) {
+                hasData = false;
+            } catch (DataAccessException e) {
+                hasData = false;
             }
+
+
+        }
         //if none of the stocks has data in the future
-        if(!hasData){
+        if (!hasData) {
             throw new GameFinishedException("Ticker");
         }
 
     }
 
-    private boolean fireEvents(){
+    private boolean fireEvents() {
 
-        boolean inCache=false;
+        boolean inCache = false;
 
         //while there are elements in the cache
-        while (dataCache.size()>0){
+        while (dataCache.size() > 0) {
             //non-destructively get the head of the queue
-            StockUpdateEvent event= dataCache.peek();
+            StockUpdateEvent event = dataCache.peek();
             //if the event has occurred in the past
-            if(event.getTime().before(currentTime)){
+            if (event.getTime().before(currentTime)) {
                 //remove that item from the queue
                 dataCache.poll();
                 //fire the event
                 eventManager.notifyListeners(event);
-                inCache=true;
+                inCache = true;
             }
             //if the rest of the events are events of the future
-            else{
+            else {
                 //do not loop anymore
                 break;
             }
 
-            }
+        }
 
-        return  inCache;
+        return inCache;
     }
 
     public ArrayList<String> getStocks() {
         return stocks;
     }
 
+    public void setDataApi(CompanyStockTransactionsData dataApi) {
+        this.dataApi = dataApi;
+    }
 }
